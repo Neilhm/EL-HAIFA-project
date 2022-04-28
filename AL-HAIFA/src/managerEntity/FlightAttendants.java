b24ca297d04abb5c003a21596f1280969b0539c3
Neilhm <neilhamzi12@gmail.com>
2022-04-28 20:36:53 +0300

    init project files

---
+++ AL-HAIFA/src/managerEntity/FlightAttendants.java
@@ -0,0 +1,19 @@
+package managerEntity;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class FlightAttendants extends Employee {
+
+	public FlightAttendants(long id, String firstName, String lastName, Date RecruitmentStartDate,
+			Date EndOfEmploymentDate) {
+		super(id, firstName, lastName, RecruitmentStartDate, EndOfEmploymentDate);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FlightAttendants(long id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
